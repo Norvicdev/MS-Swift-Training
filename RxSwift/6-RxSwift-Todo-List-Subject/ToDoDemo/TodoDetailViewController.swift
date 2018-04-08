9e99bb01602656b9dd21a088bd9ecb617405dd8a
Alex Jiang <pigflyjky@gmail.com>
2018-04-08 15:27:10 +1000

    [RxSwift](todo list subject): project import

---
+++ RxSwift/6-RxSwift-Todo-List-Subject/ToDoDemo/TodoDetailViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TodoDetailViewController.swift
+//  ToDoDemo
+//
+//  Created by Alex Jiang on 6/4/18.
+//  Copyright © 2018 Junliang Jiang. All rights reserved.
+//
+
+import UIKit
+
+class TodoDetailViewController: UITableViewController {
+    @IBOutlet weak var todoName: UITextField!
+    @IBOutlet weak var isFinished: UISwitch!
+    @IBOutlet weak var doneBarBtn: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        todoName.becomeFirstResponder()
+    }
+    
+    @IBAction func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func done() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension TodoDetailViewController {
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+}
+
+extension TodoDetailViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        
+        doneBarBtn.isEnabled = newText.length > 0
+        
+        return true
+    }
+}
