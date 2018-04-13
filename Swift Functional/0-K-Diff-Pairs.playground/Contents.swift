1829f0c03a1604df8806bdf2c6c2f73a26e8cb75
Alex Jiang <pigflyjky@gmail.com>
2018-04-13 17:01:45 +1000

    [Functional](Algorithm): k diffs in array

--- Swift Functional/0-K-Diff-Pairs.playground/Contents.swift
+++ Swift Functional/0-K-Diff-Pairs.playground/Contents.swift
@@ -33,3 +33,19 @@ isAnagramV2(a: "abc", b: "bca")
 isAnagramV2(a: "abde", b: "ebad")
 isAnagramV2(a: "123", b: "777")
 isAnagramV2(a: "badad", b: "124124")
+
+
+func kDiffs(a: [Int], k: Int) -> Int {
+    guard a.count > 1 else { return 0 }
+
+    var result = 0, lookup: [Int: Int] = [:]
+
+    a.forEach { lookup[$0, default: 0] += 1 }
+
+    lookup.keys.forEach {
+        if ((k == 0) && ( lookup[$0] > 1)) { result += 1 }
+        if k > 0 && lookup.keys.contains($0 + k) { result += 1 }
+    }
+
+    return result
+}
