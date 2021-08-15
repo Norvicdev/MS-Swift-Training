3de2e2bf6836dc170ea90026601a8bd3523e5f88
Alex Jiang <pigflyjky@gmail.com>
2021-08-15 11:24:28 +1000

    weather viewmodel init

---
+++ 08_2021/CombineWeatherApp-Starter/CombinedWeatherApp/Weather/WeeklyWeather/WeeklyWeatherView.swift
@@ -0,0 +1,15 @@
+
+import SwiftUI
+
+struct WeeklyWeatherView: View {
+  var body: some View {
+    NavigationView {
+      VStack {
+        NavigationLink(
+          "Best weather app :] ⛅️",
+          destination: CurrentWeatherView()
+        )
+      }
+    }
+  }
+}
