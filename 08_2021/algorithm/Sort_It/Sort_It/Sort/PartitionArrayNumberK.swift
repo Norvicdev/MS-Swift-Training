3f1f28a86d9b9be3d18275209662905cd8eee78f
Alex Jiang <pigflyjky@gmail.com>
2021-08-26 15:12:16 +1000

    partition array init

---
+++ 08_2021/algorithm/Sort_It/Sort_It/Sort/PartitionArrayNumberK.swift
@@ -0,0 +1,35 @@
+//
+//  PartitionArrayNumberK.swift
+//  Sort_It
+//
+//  Created by Alex Jiang on 26/8/21.
+//
+
+import Foundation
+
+//https://aaronice.gitbook.io/lintcode/array/partition_array
+//Given an array nums of integers and an int k, partition the array (i.e move the elements in "nums") such that:
+//All elements < k are moved to the left
+//All elements >= k are moved to the right
+//Return the partitioning index, i.e the first index i nums[i] >= k.
+
+extension Solution {
+  func partitionArrayNumberK(_ array: inout [Int], num: Int) -> Int {
+    guard array.count > 1 else { return 0 }
+    guard array.contains(num) else { return -1 }
+
+    var leftIdx = 0
+    var rightIdx = array.count - 1
+
+    while leftIdx < rightIdx {
+      while array[leftIdx] < num { leftIdx += 1 }
+      while array[rightIdx] > num { rightIdx -= 1 }
+
+      if leftIdx < rightIdx {
+        array.swapAt(leftIdx, rightIdx)
+      }
+    }
+
+    return leftIdx
+  }
+}
