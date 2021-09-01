9f39d6c5e0b93db017a482b9f4dddffa9586aea4
Alex Jiang <pigflyjky@gmail.com>
2021-09-01 11:14:12 +1000

    combination template init

---
+++ 08_2021/algorithm/Sort_It/Sort_It/Recursion/Combinations.swift
@@ -0,0 +1,20 @@
+//
+//  Combinations.swift
+//  Sort_It
+//
+//  Created by Alex Jiang on 1/9/21.
+//
+
+import Foundation
+
+// Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+// https://leetcode-cn.com/problems/combinations/solution/zu-he-by-leetcode-solution/
+extension Solution {
+  func combine(_ n: Int, _ k: Int) -> [[Int]] {
+    var result = [[Int]]()
+
+
+
+    return result
+  }
+}
