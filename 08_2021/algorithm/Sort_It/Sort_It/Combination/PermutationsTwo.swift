90c048ed981b80296db1746a6e203d50d871e181
Alex Jiang <pigflyjky@gmail.com>
2021-09-06 21:24:33 +1000

    permutation unique init

---
+++ 08_2021/algorithm/Sort_It/Sort_It/Combination/PermutationsTwo.swift
@@ -0,0 +1,19 @@
+//
+//  PermutationsTwo.swift
+//  Sort_It
+//
+//  Created by Alex Jiang on 6/9/21.
+//
+
+import Foundation
+
+// https://leetcode-cn.com/problems/permutations-ii/
+// Given a collection of numbers, nums, that might contain duplicates,
+// return all possible unique permutations in any order.
+extension Solution {
+  func permuteUnique(_ nums: [Int]) -> [[Int]] {
+    var result = [[Int]]()
+
+    return result
+  }
+}
