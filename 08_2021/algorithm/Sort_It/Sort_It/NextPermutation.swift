f66c8fb3c3b7f7c8987c596dadb3122a946f64b4
Alex Jiang <pigflyjky@gmail.com>
2021-09-07 22:30:09 +1000

    permutation next init

---
+++ 08_2021/algorithm/Sort_It/Sort_It/NextPermutation.swift
@@ -0,0 +1,18 @@
+//
+//  NextPermutation.swift
+//  Sort_It
+//
+//  Created by Alex Jiang on 7/9/21.
+//
+
+import Foundation
+
+// https://leetcode-cn.com/problems/next-permutation/
+// 我们需要将一个左边的「较小数」与一个右边的「较大数」交换，以能够让当前排列变大，从而得到下一个排列
+extension Solution {
+  func nextPermutation(_ nums: inout [Int]) {
+    guard !nums.isEmpty || nums.count > 1 else { return }
+
+    
+  }
+}
