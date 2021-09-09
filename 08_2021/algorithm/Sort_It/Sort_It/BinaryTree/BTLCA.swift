b81bbab3266211809f67656d0776105eff0fac8d
Alex Jiang <pigflyjky@gmail.com>
2021-09-09 10:27:01 +1000

    BT lowerst common ancestor init

---
+++ 08_2021/algorithm/Sort_It/Sort_It/BinaryTree/BTLCA.swift
@@ -0,0 +1,19 @@
+//
+//  BTLCA.swift
+//  Sort_It
+//
+//  Created by Alex Jiang on 9/9/21.
+//
+
+import Foundation
+
+// https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
+extension Solution {
+  func lowestCommonAncestor(_ root: BinaryNode<Int>?,
+                            _ p: BinaryNode<Int>?,
+                            _ q: BinaryNode<Int>?)
+  -> BinaryNode<Int>? {
+
+    return nil
+  }
+}
