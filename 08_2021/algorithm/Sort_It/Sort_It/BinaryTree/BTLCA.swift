9d8a08dd83f42388dbaa3a95b3c7de6a92d87e40
Alex Jiang <pigflyjky@gmail.com>
2021-09-11 21:00:22 +1000

    BTLCA comment

--- 08_2021/algorithm/Sort_It/Sort_It/BinaryTree/BTLCA.swift
+++ 08_2021/algorithm/Sort_It/Sort_It/BinaryTree/BTLCA.swift
@@ -8,6 +8,10 @@
 import Foundation
 
 // https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
+// https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/236-er-cha-shu-de-zui-jin-gong-gong-zu-xian-hou-xu/
+// 最近公共祖先的定义： 设节点 root 为节点 p, q 的某公共祖先，
+// 若其左子节点 root.left 和右子节点 root.right 都不是 p,q 的公共祖先，
+// 则称 root 是 “最近的公共祖先” 
 extension Solution {
   func lowestCommonAncestor(_ root: BinaryNode<Int>?,
                             _ p: BinaryNode<Int>?,
