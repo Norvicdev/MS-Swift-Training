835a35f63933f671ac0ff09fee2c670498bd3ac7
Alex Jiang <pigflyjky@gmail.com>
2021-09-11 21:30:55 +1000

    BTLCA solution

--- 08_2021/algorithm/Sort_It/Sort_It/BinaryTree/BTLCA.swift
+++ 08_2021/algorithm/Sort_It/Sort_It/BinaryTree/BTLCA.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
-// https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/236-er-cha-shu-de-zui-jin-gong-gong-zu-xian-hou-xu/
+// https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/c-jing-dian-di-gui-si-lu-fei-chang-hao-li-jie-shi-/
 // 最近公共祖先的定义： 设节点 root 为节点 p, q 的某公共祖先，
 // 若其左子节点 root.left 和右子节点 root.right 都不是 p,q 的公共祖先，
 // 则称 root 是 “最近的公共祖先” 
@@ -17,7 +17,22 @@ extension Solution {
                             _ p: BinaryNode<Int>?,
                             _ q: BinaryNode<Int>?)
   -> BinaryNode<Int>? {
+    guard let _root = root else { return nil }
+    if root?.value == p?.value || root?.value == q?.value { return root }
 
+    let left = lowestCommonAncestor(_root.leftChild, p, q)
+    let right = lowestCommonAncestor(_root.rightChild, p, q)
+
+    if left == nil {
+      return right
+    }
+    if right == nil {
+      return left
+    }
+    if left != nil && right != nil {
+      return root
+    }
+    
     return nil
   }
 }
