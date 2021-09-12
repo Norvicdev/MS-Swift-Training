38126463594ea0cd7f2756d977886edd8bae4431
Alex Jiang <pigflyjky@gmail.com>
2021-09-12 12:04:15 +1000

    min stack solution

--- 08_2021/algorithm/Sort_It/Sort_It/Stack/MinStack.swift
+++ 08_2021/algorithm/Sort_It/Sort_It/Stack/MinStack.swift
@@ -9,8 +9,11 @@ import Foundation
 
 // https://leetcode-cn.com/problems/min-stack/
 // Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
-public struct MinStack<Element> {
+// 使得每个元素 a 与其相应的最小值 m 时刻保持一一对应。因此我们可以使用一个辅助栈，
+// 与元素栈同步插入与删除，用于存储与每个元素对应的最小值。
+public struct MinStack<Element: FixedWidthInteger> {
   private var storage: [Element] = []
+  private var minStorage: [Element] = [Element.max]
 
   public init() { }
 
@@ -20,15 +23,21 @@ public struct MinStack<Element> {
 
   public mutating func push(_ element: Element) {
     storage.append(element)
+    minStorage.append(min(element, minStorage.last!))
   }
 
   @discardableResult
   public mutating func pop() -> Element? {
+    _ = minStorage.popLast()
     return storage.popLast()
   }
 
   public func peek() -> Element? {
     return storage.last
+  }
+
+  public func getMin() -> Element? {
+    return minStorage.last
   }
 
   public var isEmpty: Bool {
