410f459e8564b5a55a009e91492451c3621067f4
Alex Jiang <pigflyjky@gmail.com>
2021-09-13 19:31:58 +1000

    type eraser pub

--- 10_2021/Combine_Action/02-publishers-and-subjects/Starter.playground/Contents.swift
+++ 10_2021/Combine_Action/02-publishers-and-subjects/Starter.playground/Contents.swift
@@ -75,6 +75,49 @@ example(of: "Custom Subscriber") {
 }
 
 
+example(of: "PassThroughSubject") {
+  enum MyError: Error {
+    case test
+  }
+
+  final class StringSub: Subscriber {
+    typealias Input = String
+    typealias Failure = MyError
+
+    func receive(subscription: Subscription) {
+      subscription.request(.max(2))
+    }
+
+    func receive(_ input: String) -> Subscribers.Demand {
+      debugPrint("Received value", input)
+      return input == "PcY" ? .max(1) : .none
+    }
+
+    func receive(completion: Subscribers.Completion<MyError>) {
+      debugPrint("Received completion", completion)
+    }
+  }
+
+  let sub = StringSub()
+  let subject = PassthroughSubject<String, MyError>()
+  subject.subscribe(sub)
+
+  let subscription = subject
+    .sink(
+      receiveCompletion: { completion in
+        debugPrint("Received completion (sink)", completion)
+      },
+      receiveValue: { value in
+        debugPrint("Received value (sink)", value)
+      }
+    )
+
+  subject.send("Hello")
+  subject.send("PcY")
+  subject.send("JJP")
+}
+
+
 example(of: "Future") {
   func futureIncrement(
     integer: Int,
@@ -92,4 +135,21 @@ example(of: "Future") {
     .sink(receiveCompletion: { debugPrint($0) },
           receiveValue: { debugPrint($0) })
     .store(in: &subscriptions)
+}
+
+
+example(of: "Type erasure") {
+  // 1
+  let subject = PassthroughSubject<Int, Never>()
+
+  // 2
+  let publisher = subject.eraseToAnyPublisher()
+
+  // 3
+  publisher
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+
+  // 4
+  subject.send(0)
 }
