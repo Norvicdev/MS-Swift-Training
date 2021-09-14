f7a2529a017836972146482be2829fad3e741c05
Alex Jiang <pigflyjky@gmail.com>
2021-09-14 10:31:11 +1000

    card dealer finished

--- 10_2021/Combine_Action/02-publishers-and-subjects/challenge/Starter.playground/Contents.swift
+++ 10_2021/Combine_Action/02-publishers-and-subjects/challenge/Starter.playground/Contents.swift
@@ -19,11 +19,20 @@ example(of: "Create a Blackjack card dealer") {
     }
     
     // Add code to update dealtHand here
-    
+    if hand.points > 21 {
+      dealtHand.send(completion: .failure(.busted))
+    } else {
+      dealtHand.send(hand)
+    }
   }
   
   // Add subscription to dealtHand here
-  
+  dealtHand.sink { (completion) in
+    debugPrint("Completion", completion)
+  } receiveValue: { (value) in
+    debugPrint("Recevied Value", value)
+  }
+
   
   deal(3)
 }
