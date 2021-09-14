82f7e68d816dd64b6719d0de88dcdfd6cd38d9d5
Alex Jiang <pigflyjky@gmail.com>
2021-09-14 22:51:43 +1000

    tranform operators try map

--- 10_2021/Combine_Action/03-transforming-operators/Starter.playground/Contents.swift
+++ 10_2021/Combine_Action/03-transforming-operators/Starter.playground/Contents.swift
@@ -3,4 +3,53 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+// MARK: - Collecting values
+
+example(of: "collect") {
+  ["A", "B", "C", "D", "E"].publisher
+    .collect(3)
+    .sink(receiveCompletion: { print($0) },
+          receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+// MARK: - Mapping values
+
+example(of: "map") {
+  let formatter = NumberFormatter()
+  formatter.numberStyle = .spellOut
+
+  [96, 503, 50, 202, 157, 101].publisher
+    .map {
+      formatter.string(for: NSNumber(integerLiteral: $0)) ?? ""
+    }
+    .sink(receiveValue: { debugPrint($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "map key paths") {
+  let pub = PassthroughSubject<Coordinate, Never>()
+
+  pub
+    .map(\.x, \.y)
+    .sink { (x, y) in
+      debugPrint(
+        "The coordinate at (\(x), \(y)) is in quadrant",
+        quadrantOf(x: x, y: y)
+      )
+    }
+    .store(in: &subscriptions)
+
+  pub.send(Coordinate(x: 10, y: -8))
+  pub.send(Coordinate(x: 0, y: 5))
+}
+
+example(of: "Try Map") {
+  Just("Dir does not exist")
+    .tryMap { try
+      FileManager.default.contentsOfDirectory(atPath: $0)
+    }
+    .sink(receiveCompletion: { debugPrint($0)},
+          receiveValue: { debugPrint($0) })
+    .store(in: &subscriptions)
+}
