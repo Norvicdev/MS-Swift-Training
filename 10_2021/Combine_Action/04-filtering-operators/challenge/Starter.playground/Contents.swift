b3cb8ae9bf707b1e80442f38890db7517d3217bb
Alex Jiang <pigflyjky@gmail.com>
2021-09-15 11:34:21 +1000

    filter operators 1 to 100 done

--- 10_2021/Combine_Action/04-filtering-operators/challenge/Starter.playground/Contents.swift
+++ 10_2021/Combine_Action/04-filtering-operators/challenge/Starter.playground/Contents.swift
@@ -3,6 +3,15 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "filter") {
+  let pub = (1...100).publisher
+
+  pub
+    .drop(while: { $0 <= 50 })
+    .prefix(20)
+    .filter({ $0.isMultiple(of: 2)})
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
 
 
