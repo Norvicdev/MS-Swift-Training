6d5a36bbcad8b94b0e6da430107ab8321918f252
Alex Jiang <pigflyjky@gmail.com>
2021-09-25 12:37:15 +1000

    time based operators challenge

--- 10_2021/Combine_Action/06-time-based-operators/challenge/Starter.playground/Contents.swift
+++ 10_2021/Combine_Action/06-time-based-operators/challenge/Starter.playground/Contents.swift
@@ -4,7 +4,13 @@ import Foundation
 // A subject you get values from
 let subject = PassthroughSubject<Int, Never>()
 
-<#Add your code here#>
+let strings = subject
+  .collect(.byTime(DispatchQueue.main, .seconds(0.5)))
+  .map { array in
+    String(array.map { Character(Unicode.Scalar($0)!) }) }
+
+
+
 
 // Let's roll!
 startFeeding(subject: subject)
